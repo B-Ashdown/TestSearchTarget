9967d2da5b41bb122d5e2b32649e3b3646dad0ac
B-Ashdown <33769599+B-Ashdown@users.noreply.github.com>
2021-03-23 09:09:25 +1000

    Add files via upload

---
+++ Floor.java
@@ -0,0 +1,17 @@
+package bms.floor;
+
+import bms.exceptions.DuplicateRoomException;
+import bms.exceptions.InsufficientSpaceException;
+import bms.room.Room;
+import bms.room.RoomType;
+import bms.util.FireDrill;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Floor implements FireDrill {
+    public void addRoom(Room newRoom)
+            throws DuplicateRoomException, InsufficientSpaceException {
+        System.out.println("Hello, I may potentially be plagiarised")
+    }
+}
